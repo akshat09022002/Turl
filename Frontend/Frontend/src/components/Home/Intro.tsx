31494f6e09cc71abc6ec6781c24693cf7521f9f1
Akshat Singh <akshat09022002@gmail.com>
2025-10-09 04:11:51 +0530

    intro text fix2

--- Frontend/Frontend/src/components/Home/Intro.tsx
+++ Frontend/Frontend/src/components/Home/Intro.tsx
@@ -11,7 +11,7 @@ const Intro = () => {
             cursor: "",
           }}
           onInit={(typewriter) => {
-            typewriter.typeString("Welcome to turl").start();
+            typewriter.typeString("Welcome to Turl").start();
           }}
         />
       </div>
