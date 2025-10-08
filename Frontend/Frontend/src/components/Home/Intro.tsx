83c9b751c40a8f18d3a6757202f8b5922085c62b
Akshat Singh <akshat09022002@gmail.com>
2025-10-09 04:06:23 +0530

    intro text fixed

--- Frontend/Frontend/src/components/Home/Intro.tsx
+++ Frontend/Frontend/src/components/Home/Intro.tsx
@@ -11,7 +11,7 @@ const Intro = () => {
             cursor: "",
           }}
           onInit={(typewriter) => {
-            typewriter.typeString("WELCOME to turl").start();
+            typewriter.typeString("Welcome to turl").start();
           }}
         />
       </div>
